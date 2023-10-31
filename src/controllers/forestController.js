37265fdb77c3ab99f699a6e7811f71960d899564
mahmudulhaquequdrati <freelancermahmudqudrati@gmail.com>
2023-11-01 03:16:23 +0600

    server done

--- src/controllers/forestController.js
+++ src/controllers/forestController.js
@@ -17,23 +17,6 @@ const getAllInfo = async (req, res) => {
   }
 };
 
-<<<<<<< HEAD
-const addToMap = async (req, res) => {
-  try {
-    console.log(req.body);
-    const forest_info = new Forest(req.body);
-    const info = await forest_info.save();
-    res.status(200).json({
-      message: "New Info Added To map successfully",
-      data: info,
-    });
-  } catch (err) {
-    res.status(403).json({
-      errorMessage: "There was a problem adding info to the map",
-      error: err.message,
-    });
-  }
-=======
 // const addToMap = async (req, res) => {
 //   try {
 //     const forest_info = new Forest(req.body);
@@ -53,50 +36,55 @@ const addToMap = (req, res) => {
   upload(req, res, async (err) => {
     if (err) {
       return res.status(400).json({
-        errorMessage: 'File upload failed',
+        errorMessage: "File upload failed",
         error: err.message,
       });
     }
     console.log(req.body);
     try {
       const file = req.file;
-      const isImage = file.mimetype.startsWith('image/');
-      const isVideo = file.mimetype.startsWith('video/');
+      const isImage = file.mimetype.startsWith("image/");
+      const isVideo = file.mimetype.startsWith("video/");
+
+      let address = {
+        latitude: req.body?.latitude,
+        longitude: req.body?.longitude,
+        add: req.body?.address,
+      };
 
       if (isImage) {
         const forest_info = new Forest({
           ...req.body,
+          address: address,
           img: req.file.path,
-          video: ""
+          video: "",
         });
         const info = await forest_info.save();
         res.status(200).json({
-          message: 'New Info Added To map successfully',
+          message: "New Info Added To map successfully",
           data: info,
         });
       } else if (isVideo) {
         const forest_info = new Forest({
           ...req.body,
+          address: address,
           img: "",
           video: req.file.path,
         });
         const info = await forest_info.save();
         res.status(200).json({
-          message: 'New Info Added To map successfully',
+          message: "New Info Added To map successfully",
           data: info,
         });
       }
     } catch (err) {
       res.status(403).json({
-        errorMessage: 'There was a problem adding info to the map',
+        errorMessage: "There was a problem adding info to the map",
         error: err.message,
       });
     }
   });
->>>>>>> 700c369f260f37aa1831f8ef706f04afd514d758
 };
-
-
 
 const updateToMap = async (req, res) => {
   try {
