840822d4f28da208274c3692afa5e3d3bc6bd47a
mahmudulhaquequdrati <freelancermahmudqudrati@gmail.com>
2023-11-02 19:29:35 +0600

    done

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require("cors");
 const connectDB = require("./src/config/connectDB");
 const errorHandler = require("./src/middleware/errorHandler");
 const PORT = process.env.PORT || 5000;
+const bodyParser = require("body-parser");
 const app = express();
 
 const forestRoute = require("./src/routes/forestRoute");
@@ -15,6 +16,8 @@ app.use(cors());
 app.use(express.json());
 connectDB();
 colors.enable();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send(
@@ -27,7 +30,7 @@ app.get("/", (req, res) => {
 // -----
 // Routes
 app.use("/api", forestRoute);
-app.use('/uploads', express.static('uploads'));
+app.use("/uploads", express.static("uploads"));
 // -----
 // ----------
 // ---------------
--- src/controllers/forestController.js
+++ src/controllers/forestController.js
@@ -33,73 +33,45 @@ const getAllInfo = async (req, res) => {
 //   }
 // };
 const addToMap = async (req, res) => {
-  upload(req, res, async (err) => {
-    // console.log(req);
-    if (err) {
-      // return res.status(400).json({
-      //   errorMessage: "File upload failed",
-      //   error: err.message,
-      // });
-    }
-    try {
-      const file = req.file;
-      let isImage = null;
-      let isVideo = null;
-      if (file) {
-        isImage = file.mimetype.startsWith("image/");
-        isVideo = file.mimetype.startsWith("video/");
-      }
+  // upload(req, res, async (err) => {
+  //   // console.log(req);
+  //   if (err) {
+  //     // return res.status(400).json({
+  //     //   errorMessage: "File upload failed",
+  //     //   error: err.message,
+  //     // });
+  //   }
 
-      let address = {
-        latitude: req.body?.latitude,
-        longitude: req.body?.longitude,
-        add: req.body?.address,
-      };
+  // });
 
-      if (isImage) {
-        const forest_info = new Forest({
-          ...req.body,
-          address: address,
-          img: req.file.path,
-          video: "",
-        });
-        const info = await forest_info.save();
-        res.status(200).json({
-          message: "New Info Added To map successfully",
-          data: info,
-        });
-      } else if (isVideo) {
-        const forest_info = new Forest({
-          ...req.body,
-          address: address,
-          img: "",
-          video: req.file.path,
-        });
-        const info = await forest_info.save();
-        res.status(200).json({
-          message: "New Info Added To map successfully",
-          data: info,
-        });
-      } else {
-        const forest_info = new Forest({
-          ...req.body,
-          address: address,
-          img: "",
-          video: "",
-        });
-        const info = await forest_info.save();
-        res.status(200).json({
-          message: "New Info Added To map successfully",
-          data: info,
-        });
-      }
-    } catch (err) {
-      res.status(403).json({
-        errorMessage: "There was a problem adding info to the map",
-        error: err.message,
-      });
-    }
-  });
+  try {
+    // const file = req.file;
+    // let isImage = null;
+    // let isVideo = null;
+    // if (file) {
+    //   isImage = file.mimetype.startsWith("image/");
+    //   isVideo = file.mimetype.startsWith("video/");
+    // }
+    let address = {
+      latitude: req.body?.latitude,
+      longitude: req.body?.longitude,
+      add: req.body?.address,
+    };
+    const forest_info = new Forest({
+      ...req.body,
+      address: address,
+    });
+    const info = await forest_info.save();
+    res.status(200).json({
+      message: "New Info Added To map successfully",
+      data: info,
+    });
+  } catch (err) {
+    res.status(403).json({
+      errorMessage: "There was a problem adding info to the map",
+      error: err.message,
+    });
+  }
 };
 
 const updateToMap = async (req, res) => {
